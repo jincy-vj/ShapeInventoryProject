7d2fd620a36be9b9711f855c352a82c28ed1b51f
vazhaji <jincy.vj@accenture.com>
2025-11-26 17:20:12 +0200

    Initial commit: Java Shape Inventory project with GoF patterns

---
+++ src/Circle.java
@@ -0,0 +1,17 @@
+public class Circle extends Shape {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public String toString() {
+        return "Circle(radius=" + radius + ")";
+    }
+
+}
